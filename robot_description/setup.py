6fb0aed80a384e67c3f1dec349c282ef04ffb902
sam-ovalle <samovalle878@gmail.com>
2025-06-23 13:30:15 +0000

    exercise 2.3.1 complete: meshes for chassis and wheels included in /meshes, setup.py, and URDF visual elements

--- robot_description/setup.py
+++ robot_description/setup.py
@@ -15,6 +15,7 @@ setup(
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*.stl')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
